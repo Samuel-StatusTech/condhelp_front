12654092d1670d291252e3cc5c3043b98a7ccaae
SamuelBrunoo <samuel.k.bruno@gmail.com>
2025-01-24 17:30:29 -0300

    Franchise MyAccount multiple fields

--- src/pages/MyAccount/contents/franchise.tsx
+++ src/pages/MyAccount/contents/franchise.tsx
@@ -4,6 +4,16 @@ import Form from "../../../components/Form"
 import PageHeader from "../../../components/PageHeader"
 import { TBlock } from "../../../utils/@types/components/Form"
 import { formatPhone } from "../../../utils/tb/format/phone"
+import { TErrorsCheck } from "../../../utils/@types/helpers/checkErrors"
+import { formatCep } from "../../../utils/tb/format/cep"
+import { TCity } from "../../../utils/@types/data/region"
+import { TOption } from "../../../components/Input/condosSelect"
+import {
+  formatCityInscription,
+  formatStateInscription,
+} from "../../../utils/tb/format/inscription"
+import { formatCNPJ } from "../../../utils/tb/format/cnpj"
+import { formatCpf } from "../../../utils/tb/format/cpf"
 
 type Props = {
   handleField: (field: string, value: any) => void
@@ -11,6 +21,13 @@ type Props = {
   handleSave: (form: any) => Promise<void>
   form: any
   formSubmitFields: TBlock["groups"][number]
+
+  handleSelectCity: (city: TCity) => void
+
+  options: {
+    [key: string]: TOption[]
+  }
+  errors: TErrorsCheck
 }
 
 const MyAccountFranchise = (props: Props) => {
@@ -21,6 +38,11 @@ const MyAccountFranchise = (props: Props) => {
 
     form,
     formSubmitFields,
+
+    handleSelectCity,
+
+    options,
+    errors,
   } = props
 
   return (
@@ -48,8 +70,159 @@ const MyAccountFranchise = (props: Props) => {
                           value: form.name,
                           placeholder: "Digite aqui",
                           gridSizes: { big: 3, small: 12 },
+                          error: {
+                            has: errors.fields.includes("name"),
+                            message: "Digite um nome",
+                          },
                         },
                       ],
+                    ],
+                  },
+
+                  // Address
+                  {
+                    type: "fields",
+                    fields: [
+                      [
+                        {
+                          type: "select",
+                          label: "País",
+                          placeholder: "País",
+                          field: "country",
+                          value: form.address?.country ?? "",
+                          options: options.country,
+                          gridSizes: { big: 3, small: 6 },
+                          elevation: 10,
+                          error: {
+                            has: errors.fields.includes("country"),
+                            message: "Selecione um país",
+                          },
+                        },
+                        {
+                          type: "select",
+                          label: "Estado",
+                          placeholder: "Estado",
+                          field: "state",
+                          value: form.address?.state ?? ("" as string),
+                          options: options.state,
+                          gridSizes: { big: 3, small: 6 },
+                          error: {
+                            has: errors.fields.includes("state"),
+                            message: "Selecione um estado",
+                          },
+                        },
+                        {
+                          type: "cityInput",
+                          label: "Cidade",
+                          field: "city",
+                          placeholder: "Digite aqui",
+                          value: form.address.city,
+                          gridSizes: { big: 6, small: 12 },
+                          stateId: form.address.state,
+                          onSelectCity: handleSelectCity,
+                          error: {
+                            has: errors.fields.includes("city"),
+                            message: "Selecione uma cidade",
+                          },
+                        },
+                      ],
+                      [
+                        {
+                          type: "input",
+                          field: "street",
+                          label: "Endereço",
+                          value: form.address?.street ?? "",
+                          placeholder: "Digite aqui",
+                          gridSizes: { big: 8, small: 7 },
+                          error: {
+                            has: errors.fields.includes("street"),
+                            message: "Digite o nome da rua",
+                          },
+                        },
+                        {
+                          type: "input",
+                          field: "number",
+                          label: "Número",
+                          value: form.address?.number ?? "",
+                          placeholder: "0",
+                          gridSizes: { big: 4, small: 5 },
+                          error: {
+                            has: errors.fields.includes("number"),
+                            message: "Digite o número da rua",
+                          },
+                        },
+                      ],
+                      [
+                        {
+                          type: "input",
+                          field: "complement",
+                          label: "Complemento",
+                          value: form.address?.complement ?? "",
+                          placeholder: "Digite aqui",
+                          gridSizes: { big: 8, small: 7 },
+                          error: {
+                            has: false,
+                            message: "Digite um complemento",
+                          },
+                        },
+                        {
+                          type: "input",
+                          field: "zipCode",
+                          label: "CEP",
+                          value: formatCep(form.address?.zipCode ?? ""),
+                          placeholder: "00000-000",
+                          gridSizes: { big: 4, small: 5 },
+                          error: {
+                            has: errors.fields.includes("zipCode"),
+                            message: "Digite o CEP",
+                          },
+                        },
+                      ],
+                    ],
+                  },
+
+                  {
+                    type: "fields",
+                    fields: [
+                      [
+                        {
+                          type: "input",
+                          field: "phone1",
+                          label: "Telefone principal com DDD",
+                          value: formatPhone(form.phone1),
+                          placeholder: "00 00000-0000",
+                          gridSizes: { big: 6, small: 12 },
+                          error: {
+                            has: errors.fields.includes("phone1"),
+                            message: "Telefone principal obrigatório",
+                          },
+                        },
+                        {
+                          type: "input",
+                          field: "phone2",
+                          label: "Telefone 2",
+                          value: formatPhone(form.phone2),
+                          placeholder: "00 00000-0000",
+                          gridSizes: { big: 6, small: 12 },
+                          error: {
+                            has: errors.fields.includes("phone2"),
+                            message: "Digite um telefone válido",
+                          },
+                        },
+                      ],
+                      {
+                        type: "readonly",
+                        label: "Email",
+                        field: "email",
+                        value: form.email,
+                        gridSizes: { big: 12 },
+                      },
+                    ],
+                  },
+
+                  {
+                    type: "fields",
+                    fields: [
                       {
                         type: "profile",
                         label: "Imagem de perfil",
@@ -66,31 +239,129 @@ const MyAccountFranchise = (props: Props) => {
           {
             blocks: [
               {
-                title: "Informações de contato",
+                title: "Informações do perfil",
                 groups: [
                   {
                     type: "fields",
-                    fields: [
-                      [
-                        {
-                          type: "input",
-                          field: "phone1",
-                          label: "Telefone principal com DDD",
-                          value: formatPhone(form.phone1),
-                          placeholder: "00 00000-0000",
-                          gridSizes: { big: 6, small: 12 },
-                        },
-                        {
-                          type: "input",
-                          field: "phone2",
-                          label: "Telefone 2",
-                          value: formatPhone(form.phone2),
-                          placeholder: "00 00000-0000",
-                          gridSizes: { big: 6, small: 12 },
-                        },
-                      ],
-                    ],
+                    // @ts-ignore
+                    fields: form.responsible
+                      ? [
+                          {
+                            type: "radio",
+                            field: "responsableResponsibleType",
+                            value: form.responsible.responsibleType,
+                            gridSizes: { big: 12 },
+                            options: [
+                              { key: "CNPJ", value: "Pessoa Jurídica" },
+                              { key: "CPF", value: "Pessoa Física" },
+                            ],
+                          },
+                          ...(form.responsible.responsibleType === "CNPJ"
+                            ? [
+                                {
+                                  type: "input",
+                                  field: "responsableCompanyName",
+                                  label: "Pessoa Jurídica responsável",
+                                  value: form.responsible.companyName,
+                                  placeholder: "Informe a razão social",
+                                  gridSizes: { big: 12 },
+                                  error: {
+                                    has: errors.fields.includes(
+                                      "responsableCompanyName"
+                                    ),
+                                    message: "Digite a razão social",
+                                  },
+                                },
+                                [
+                                  {
+                                    type: "input",
+                                    field: "responsableFantasyName",
+                                    label: "Nome fantasia",
+                                    value: form.responsible.fantasyName,
+                                    placeholder: "Informe o nome fantasia",
+                                    gridSizes: { big: 6, small: 12 },
+                                    error: {
+                                      has: errors.fields.includes(
+                                        "responsableFantasyName"
+                                      ),
+                                      message: "Digite o nome fantasia",
+                                    },
+                                  },
+                                  {
+                                    type: "input",
+                                    field: "responsableCnpj",
+                                    label: "CNPJ",
+                                    value: formatCNPJ(form.responsible.cnpj),
+                                    placeholder: "00.000.000/0001-00",
+                                    gridSizes: { big: 6, small: 12 },
+                                    error: {
+                                      has: errors.fields.includes(
+                                        "responsableCnpj"
+                                      ),
+                                      message: "Digite o CNPJ",
+                                    },
+                                  },
+                                ],
+                                [
+                                  {
+                                    type: "input",
+                                    field: "responsableStateRegistration",
+                                    label: "Inscrição estadual",
+                                    value: formatStateInscription(
+                                      form.responsible.stateRegistration
+                                    ),
+                                    placeholder: "Apenas números",
+                                    gridSizes: { big: 6, small: 12 },
+                                  },
+                                  {
+                                    type: "input",
+                                    field: "responsableMunicipalRegistration",
+                                    label: "Inscrição municipal",
+                                    value: formatCityInscription(
+                                      form.responsible.municipalRegistration
+                                    ),
+                                    placeholder: "Apenas números",
+                                    gridSizes: { big: 6, small: 12 },
+                                  },
+                                ],
+                              ]
+                            : [
+                                [
+                                  {
+                                    type: "input",
+                                    field: "responsablePersonName",
+                                    label: "Pessoa responsável",
+                                    value: form.responsible.personName,
+                                    placeholder: "Informe o nome da pessoa",
+                                    gridSizes: { big: 6, small: 12 },
+                                    error: {
+                                      has: errors.fields.includes(
+                                        "responsablePersonName"
+                                      ),
+                                      message:
+                                        "Digite o nome do(a) responsável",
+                                    },
+                                  },
+                                  {
+                                    type: "input",
+                                    field: "responsableCpf",
+                                    label: "CPF",
+                                    placeholder: "123.456.789-10",
+                                    value: formatCpf(form.responsible.cpf),
+                                    gridSizes: { big: 6, small: 12 },
+                                    error: {
+                                      has: errors.fields.includes(
+                                        "responsableCpf"
+                                      ),
+                                      message: "Digite o cpf do(a) responsável",
+                                    },
+                                  },
+                                ],
+                              ]),
+                        ]
+                      : [],
                   },
+
                   formSubmitFields,
                 ],
               },
